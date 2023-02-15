be1b41625affd99070db0ac37ec8ce8488024c37
Brian James Celine <brianceline@gmail.com>
2023-02-15 16:32:54 +0000

    New font added Great Vibes

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -7,10 +7,13 @@ export default function Success() {
         Success Stories...
       </div>
       <div className="ml-5 mr-5 font-montserrat font-light text-[18px] text-left text-slate">
-        <div className="mb-5 pl-10 pr-10 box-border ">
-          I have seen, for the first time ever, groups having positive and open
-          discussions about mental health as a result of the partnership with I
-          Am Here. The bigger out support network, the better.
+        <div className="border border-emerald-500 m-5 p-5 rounded-lg">
+          <p>''</p>
+          <p>
+            I have seen, for the first time ever, groups having positive and
+            open discussions about mental health as a result of the partnership
+            with I Am Here. The bigger out support network, the better.
+          </p>
         </div>
         <div className="mb-5 pl-10 pr-10">
           I have seen, for the first time ever, groups having positive and open
