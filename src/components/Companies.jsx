7285b0541de0e13da524780313d2c9517e6a8cb4
Brian James Celine <brianceline@gmail.com>
2023-02-28 17:30:43 +0000

    Comopanies image edit

--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 export default function Companies() {
   return (
     <div>
-      <p className="font-Montserrat text-center ml-5 mr-5 mb-10">
+      <p className="font-Montserrat text-lg text-center ml-5 mr-5 mb-5">
         Companies I have worked with...
       </p>
-      <div className="flex flex-wrap ml-5 mr-5">
-        <img src="/images/Virgin.jpg" />
-        <img src="/images/David_Lloyd.jpg" />
-        <img src="/images/Fitness_First.jpg" />
-        <img src="/images/Esporta.jpg" />
-        <img src="/images/Inner_Beauty.jpg" />
-        <img src="/images/Soho_Gyms.jpg" />
-        <img src="/images/Bedford_House.jpg" />
-        <img src="/images/Places_Leisure.jpg" />
+      <div className="flex flex-wrap justify-evenly ml-2 mr-2 mb-0 pt-5">
+        <img className="pb-10" src="/images/Virgin.jpg" />
+        <img className="pb-10" src="/images/David_Lloyd.jpg" />
+        <img className="pb-10" src="/images/Fitness_First.jpg" />
+        <img className="pb-10" src="/images/Esporta.jpg" />
+        <img className="pb-10" src="/images/Inner_Beauty.jpg" />
+        <img className="pb-10" src="/images/Soho_Gyms.jpg" />
+        <img className="pb-10" src="/images/Bedford_House.jpg" />
+        <img className="pb-10" src="/images/Places_Leisure.jpg" />
       </div>
     </div>
   );
